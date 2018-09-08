8a316ecc2ddfc5c06873860b6bbc2206b788aa95
joaopaulo-ferreira <joaopaulof.soares@gmail.com>
2018-09-08 01:40:51 -0300

    .

--- Cinema/main.cpp
+++ Cinema/main.cpp
@@ -1,9 +1,94 @@
 #include <iostream>
-
+#include <vector>
+#include <sstream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+struct Cliente{
+    string id;
+    string fone;
+    Cliente(string id = "fulano", string fone = "0000"){
+        this->id = id;
+        this->fone = fone;
+    }
+    string toString(){
+        stringstream ss;
+        ss << this->id << ":" << this->fone;
+        return ss.str();
+    }
+};
+
+struct Sala{
+    vector< Cliente * > cadeiras;
+
+    Sala(int qtd = 0){
+        for(int i = 0; i < qtd; i++)
+            cadeiras.push_back(nullptr);
+    }
+
+    bool existe(int id){
+        if(cadeiras[id] != nullptr)
+            return true;
+        return false;
+    }
+
+    bool reservar(Cliente *cliente, int ind){
+
+        int qtd = cadeiras.size();
+        if((ind < 0) || (ind >= qtd)){
+            cout << "fail: essa cadeira nao existe" << endl;
+            return false;
+        }
+
+        if(existe(ind)){
+            cout << "fail: essa cadeira ja esta ocupada" << endl;
+            return false;
+        }
+
+        for(int i = 0; i < (int)cadeiras.size(); i++){
+            if(existe(i) && (cadeiras[i]->id) == (cliente->id)){
+                cout << "fail: voce ja esta no cinema" << endl;
+                return false;
+            }
+        }
+//        cout << cadeiras[0]->id << " == " << cliente.id << endl;
+        cadeiras[ind] = cliente;
+//        cout << "out reservar" <<endl;
+        return true;
+    }
+
+    string toString(){
+        stringstream ss;
+        ss << "[ ";
+        for ( int i = 0; i < (int)cadeiras.size(); i++){
+            ss << cadeiras[i]->toString() << " ";
+//            cout << cadeiras[i]->id << endl;
+
+        }
+        ss << "]";
+        cout  << "help" << endl;
+        return ss.str();
+    }
+};
+
+int main(){
+    Sala sala;
+    string op;
+    while(true){
+        cin >> op;
+        if(op == "show"){
+            cout << sala.toString() << endl;
+        }else if(op == "init"){
+            int qtd;
+            cin >> qtd;
+            sala = Sala(qtd);
+            cout << "sala criada com " << qtd << " cadeiras" << endl;
+        }else if(op == "r"){
+            string nome, fone;
+            int ind;
+            cin >> nome >> fone >> ind;
+            Cliente c (nome,fone);
+            if(sala.reservar(&c,ind))
+                cout << "done" << endl;
+        }
+    }
 }
--- Topic/ptrTopic/main.cpp
+++ Topic/ptrTopic/main.cpp
@@ -1,9 +1,186 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <cstdio>
 
+#define INVALID -1
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+struct Cliente{
+    string id;
+    int idade;
+    Cliente(string id = "lero", int idade = 0){
+        this->id = id;
+        this->idade = idade;
+    }
+
+    string toString(){
+        stringstream f;
+    }
+};
+
+
+struct Topic{
+    int lot;
+    int pref;
+    vector<Cliente *> cadeiras;
+    
+    Topic(int lot,int pref) : cadeiras(lot, nullptr)/*, lot(lot), pref(pref)*/{
+    }
+
+
+
+
+
+    string toString(){
+        stringstream s;
+        s << "[ ";
+        for(int i = 0; i < lot; i++){
+            if(cadeiras[i] == nullptr){
+                i < pref ? s << " @ " : s << " = ";
+            }else{
+
+            }
+
+        }
+        s << "]";
+        return s.str();
+    }
+
+//    int getNormalIndex(){
+//        for(int i = pref; i < lot; i++){
+////            cout << "normal index " << "pref = " << pref << "lot = " << lot << "i = " << i << endl;
+//            if(cadeiras[i].estado == '=')
+//                return i;
+//        }
+//        return INVALID;
+//    }
+
+//    int getPriorIndex(){
+//        for(int i = 0; i < pref; i++){
+//            if(cadeiras[i].estado == '@')
+//                return i;
+//        }
+//        return INVALID;
+//    }
+
+//    bool alreadyIS(string nome){
+//        for(int i = 0; i < lot; i++){
+//            if(cadeiras[i].cliente.id == nome)
+//                return true;
+//        }
+//        return false;
+//    }
+
+//    bool insert(string id, int idade){
+
+//        int index;
+//        if(idade < 60){
+//            index = getNormalIndex();
+//            if(index != INVALID){
+//                cadeiras[index] = Cadeira('x');
+//                cadeiras[index].cliente = Cliente(id, idade);
+//                return true;
+//            }
+//            index = getPriorIndex();
+//            if(index != INVALID){
+//                cadeiras[index] = Cadeira('x');
+//                cadeiras[index].cliente = Cliente(id, idade);
+//                return true;
+//            }
+//        }
+
+//        if(idade >= 60){
+//            index = getPriorIndex();
+//            if(index != INVALID){
+//                cadeiras[index] = Cadeira('x');
+//                cadeiras[index].cliente = Cliente(id, idade);
+//                return true;
+//            }
+//            index = getNormalIndex();
+//            if(index != INVALID){
+//                cadeiras[index] = Cadeira('x');
+//                cadeiras[index].cliente = Cliente(id, idade);
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+//    bool remove(string nome){
+//        for(int i = 0; i < lot; i++){
+//            if(cadeiras[i].cliente.id == nome){
+//                i < pref ? cadeiras[i].estado = '@' : cadeiras[i].estado = '=';
+//                cadeiras[i].cliente = Cliente();
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+};
+
+
+
+struct Controller{
+//    Topic topic;
+
+//    string shell(string request){
+//        stringstream in(request);
+//        stringstream out;
+//        string op;
+//        in >> op;
+
+//        if( op == "help"){
+//            out << "new <int><int>; show; in <str><int>; out<str>";
+//        }
+//        if( op == "new"){
+//            int lot, pri;
+//            in >> lot >> pri;
+//            topic = Topic(lot, pri);
+//            out << "done";
+//        }
+
+//        if( op == "in"){
+
+//            string nome;
+//            int idade;
+//            in >> nome >> idade;
+//            if(!topic.alreadyIS(nome))
+//                topic.insert(nome, idade)? out << "done" : out << "fail: Topic lotada";
+//            else
+//                out << "fail: " << nome << " ja esta na topic";
+//        }
+
+//        if(op == "show")
+//            out << topic.toString();
+
+//        if(op == "out"){
+//            string nome;
+//            in >> nome;
+//            topic.remove(nome)? out << "done" : out << "fail: " << nome << " nao está na topic";
+//        }
+
+
+//        return out.str();
+//    }
+
+    void exec(){
+//        string line;
+//        while(true){
+//            getline(cin, line);
+//            if(line == "end")
+//                    return;
+//            cout << "    " << shell(line)<< endl;
+
+//        }
+    }
+
+
+};
+
+int main(){
+    cout << "stating ..." << endl;
+    Controller c;
+    c.exec();
     return 0;
 }
